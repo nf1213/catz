a561b4ee3c9813213f6f6ebe5c29cda66314bb17
Nicole <nicole@mustbin.com>
2016-01-30 20:48:36 -0500

    Cat deletion

--- app/src/main/java/com/github/nf1213/catz/CatViewActivity.java
+++ app/src/main/java/com/github/nf1213/catz/CatViewActivity.java
@@ -1,7 +1,10 @@
 package com.github.nf1213.catz;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.Arrays;
@@ -22,13 +25,14 @@ public class CatViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_view);
 
+        final Realm realm = Realm.getInstance(this);
+
         name = (TextView) findViewById(R.id.name);
         gender = (TextView) findViewById(R.id.gender);
         description = (TextView) findViewById(R.id.description);
 
         if (getIntent() != null) {
             int catId = getIntent().getIntExtra(CAT_ID, -1);
-            Realm realm = Realm.getInstance(this);
             cat = realm.where(Cat.class)
                     .equalTo("id", catId)
                     .findFirst();
@@ -37,5 +41,16 @@ public class CatViewActivity extends AppCompatActivity {
             gender.setText(Arrays.asList(getResources().getStringArray(R.array.genders)).get(cat.getGender()));
             description.setText(cat.getDescription());
         }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                realm.beginTransaction();
+                cat.removeFromRealm();
+                realm.commitTransaction();
+                finish();
+            }
+        });
     }
 }
